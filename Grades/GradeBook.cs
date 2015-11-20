4961be3b139ff74090757a0273e180f7168f6738
'Kostas <kostas.siabanis@gmail.com>
2015-11-20 15:09:24 +0200

    Initial commit

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        // This is a constructor
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            return new GradeStatistics();
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades;
+    }
+}
